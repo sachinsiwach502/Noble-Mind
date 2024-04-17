1da048dabb0b5f730a75ffe433b7f903dbeca932
sachinsiwach502 <sachinsiwach502@Gmail.com>
2024-04-17 12:35:05 +0530

    add scroll to top

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,40 @@
 let menuIcon = document.querySelector(".menubar");
 let menuList = document.querySelector(".menuList");
+let body = document.body;
 
 menuIcon.addEventListener("click", () => {
-    menuList.classList.toggle("show")
-})
+    menuList.classList.toggle("show");
+    if (menuList.classList.contains("show")) {
+        body.style.overflow = "hidden"
+    } else {
+        body.style.overflow = "auto";
+    }
+});
+
+let navLinks = document.querySelectorAll(".nav-links");
+navLinks.forEach(items => {
+    items.addEventListener("click", () => {
+        menuList.classList.remove("show");
+        body.style.overflow = "auto";
+    })
+});
+
+//---------------------- scroll to top
+let scroll_top = document.querySelector(".scroll_to_top");
+let scrol_pt = 200;
+scroll_top.style.display = "none";
+window.addEventListener("scroll", () => {
+    if (window.scrollY > scrol_pt) {
+        scroll_top.style.display = "block"
+    } else {
+        scroll_top.style.display = "none";
+    }
+});
+
+scroll_top.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0, behaviour: "smooth",
+    })
+});
+
+
